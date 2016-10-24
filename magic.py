e544816c4653a72b52da70386c44d83092fa77a9
ashura <649717518@qq.com>
2016-10-25 07:39:10 +0800

    final version

--- magic.py
+++ magic.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 # import Image
 from PIL import Image
 
-def deal_image(input, x1, y1, x2, y2):
+def deal_image(input, x1, y1, x2, y2,size):
 	fname = input.split('/')[-1]
 	a = scipy.misc.imread(input)
 	#crop x1, y1, x2, y2
@@ -12,14 +12,19 @@ def deal_image(input, x1, y1, x2, y2):
 	box = (int(x1),int(y1),int(x2),int(y2))
 	print box
 	img = im.crop(box)
-	inp = img.resize((128, 128))
+	#inp = img.resize((32, 32))
+	inp = img.resize((int(size), int(size)))
 	#img.save('subpixel/data/celebA/test/'+'xyz.jpg', inp)
 	fname = x1+'-'+y1+'-'+x2+'-'+y2+'-'+fname
 	tmp_path = 'tmp/'+fname
 	inp.save(tmp_path,'png')
 	#transfer 
-	os.system('python main.py --is_single True --file_name %s' % fname)
-	# os.system('rm tmp/*')
+	print"size=",int(size)
+	if int(size) == 128:
+		os.system('python main.py --is_single False --is_small False --file_name %s' % fname)
+	else :
+		os.system('python main.py --is_single True  --file_name %s' % fname)
+	os.system('rm tmp/*')
 	#outpath = transfer(tmp_path)
 	outpath="out_%s" % fname
 	print outpath
